11a5e4d08138958f1881efcdf947a6b8344fff4f
Colin But <colin.but@outlook.com>
2021-12-21 19:21:36 +0000

    first commit

---
+++ src/main/java/com/example/vaultspringboot/VaultSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.vaultspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaultSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VaultSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/vaultspringboot/VaultSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vaultspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaultSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
