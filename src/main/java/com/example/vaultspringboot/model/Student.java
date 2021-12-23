cc3392d17d981814ee1de24cc8744d5441cad5ad
Colin But <colin.but@outlook.com>
2021-12-23 13:41:41 +0000

    JPA entities modelling plus configuring logging with logback

--- src/main/java/com/example/vaultspringboot/model/Student.java
+++ src/main/java/com/example/vaultspringboot/model/Student.java
@@ -1,10 +1,28 @@
 package com.example.vaultspringboot.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 import lombok.Data;
 
 @Data
+@Entity(name = "students")
+@Table(name = "students")
 public class Student {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    
+    @Column(name = "firstname", length = 20, nullable = false)
     private String firstName;
+
+    @Column(name = "lastname", length = 20, nullable = false)
     private String lastName;
+    
     private int age;
 }
